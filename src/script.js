93f828e149448b6475cb43c56a140992f4d4f98a
Muna <111438851+munamenuga@users.noreply.github.com>
2023-12-07 15:03:11 +0100

    Added search functionality using the API
    Added search functionality and edited CSS

--- src/script.js
+++ src/script.js
@@ -1,10 +1,26 @@
-function submitSearch (event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "9fa92c842dt3d47c15b7b44f3cd0od45";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
 
+searchCity("Ottawa");
